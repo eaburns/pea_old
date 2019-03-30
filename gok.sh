b3834cbb443242dda001a4f8eccc0d1ceaad8951
Ethan Burns <burns.ethan@gmail.com>
2019-03-30 19:49:42 -0400

    add gok.sh

---
+++ gok.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+o=$(mktemp tmp.XXXXXXXXXX)
+
+fail() {
+	echo Failed
+	cat $o | grep -v deprecated
+	rm $o
+	exit 1
+}
+
+trap fail INT TERM
+
+echo gofmt
+gofmt -s -l $(find . -name '*.go') > $o 2>&1
+test $(wc -l $o | awk '{ print $1 }') = "0" || fail
+
+echo govet
+go vet ./... > $o 2>&1 || fail
+
+echo ineffassign
+ineffassign . \
+	| grep -v "grammar.go:" \
+	> $o 2>&1
+e=$(mktemp tmp.XXXXXXXXXX)
+touch $e
+diff $o $e > /dev/null || { rm $e; fail; }
+rm $e
+
+echo misspell
+misspell . > $o 2>&1 || fail
+
+echo gocyclo
+gocyclo -over 15 .\
+	| grep -v "grammar.go:" \
+	> $o 2>&1
+e=$(mktemp tmp.XXXXXXXXXX)
+touch $e
+diff $o $e > /dev/null || { rm $e; fail; }
+rm $e
+
+echo go test
+go test -test.timeout=60s ./... > $o 2>&1 || fail
+
+echo golint
+golint ./... \
+	| grep -v "grammar.go:" \
+	> $o 2>&1
+# Silly: diff the grepped golint output with empty.
+# If it's non-empty, error, otherwise succeed.
+e=$(mktemp tmp.XXXXXXXXXX)
+touch $e
+diff $o $e > /dev/null || { rm $e; fail; }
+rm $e
+
+rm $o
