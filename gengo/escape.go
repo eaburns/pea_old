819fbfa15331a042dadd03381664be65a548fa00
Ethan Burns <burns.ethan@gmail.com>
2020-01-01 10:47:48 -0500

    add a function to escape arbitrary strings into valid Go identifiers

---
+++ gengo/escape.go
@@ -0,0 +1,78 @@
+package gengo
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+// escape returns an escaped form of a string suitable for a Go identifier.
+// The escape character is _.
+// Any byte other than [a-zA-Z0-9] is escaped in the form _XX
+// where XX are the hex digits of the byte.
+func escape(s string, out *strings.Builder) *strings.Builder {
+	for _, r := range s {
+		if azAZ09(r) {
+			out.WriteRune(r)
+			continue
+		}
+		var bs [utf8.UTFMax]byte
+		n := utf8.EncodeRune(bs[:], r)
+		for _, b := range bs[:n] {
+			out.WriteRune('_')
+			out.WriteRune(hexDigit(b >> 4))
+			out.WriteRune(hexDigit(b & 0xF))
+		}
+	}
+	return out
+}
+
+// unescape returns the unescaped version of a strings escaped by escape().
+func unescape(s string) (string, error) {
+	var out strings.Builder
+	for len(s) > 0 {
+		r, w := utf8.DecodeRuneInString(s)
+		s = s[w:]
+		if r != '_' {
+			out.WriteRune(r)
+			continue
+		}
+		d0, w := utf8.DecodeRuneInString(s)
+		s = s[w:]
+		n0, err := hexVal(d0)
+		if err != nil {
+			return "", err
+		}
+		d1, w := utf8.DecodeRuneInString(s)
+		s = s[w:]
+		n1, err := hexVal(d1)
+		if err != nil {
+			return "", err
+		}
+		out.WriteByte(n0<<4 | n1)
+	}
+	return out.String(), nil
+}
+
+func hexVal(d rune) (byte, error) {
+	b := byte(d & 0xFF)
+	if 'A' <= b && b <= 'F' {
+		return b - ('A' - 10), nil
+	}
+	if '0' <= b && b <= '9' {
+		return b - '0', nil
+	}
+	return 0, fmt.Errorf("bad hex digit: %c", d)
+}
+
+func hexDigit(b byte) rune {
+	r := rune(b & 0xF)
+	if r < 10 {
+		return r + '0'
+	}
+	return r + ('A' - 10)
+}
+
+func azAZ09(r rune) bool {
+	return 'a' <= r && r <= 'z' || 'A' <= r && r <= 'Z' || '0' <= r && r <= '9'
+}
---
+++ gengo/escape_test.go
@@ -0,0 +1,55 @@
+package gengo
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestEscape(t *testing.T) {
+	tests := []struct {
+		str  string
+		want string
+	}{
+		{"", ""},
+		{"a", "a"},
+		{"noNeedToEscape", "noNeedToEscape"},
+		{" ", "_20"},
+		{"_", "_5F"},
+		{"abc_xyz", "abc_5Fxyz"},
+		{"abc/xyz", "abc_2Fxyz"},
+		{"☺", "_E2_98_BA"},
+		{"こんにちは", "_E3_81_93_E3_82_93_E3_81_AB_E3_81_A1_E3_81_AF"},
+	}
+	for _, test := range tests {
+		got := escape(test.str, new(strings.Builder)).String()
+		if got != test.want {
+			t.Errorf("escape(%q)=%q, want %q", test.str, got, test.want)
+			continue
+		}
+		got1, err := unescape(got)
+		if err != nil {
+			t.Errorf("unescape(%q)=_,%v, want %q,nil", got, err, test.str)
+			continue
+		}
+		if got1 != test.str {
+			t.Errorf("unescape(%q)=%q, want %q", got, got1, test.str)
+			continue
+		}
+	}
+}
+
+func TestUnescapeBadInput(t *testing.T) {
+	tests := []string{
+		"_",
+		"_F",
+		"_ff",
+		"_GA",
+		"_0G",
+		"_☺",
+	}
+	for _, test := range tests {
+		if _, err := unescape(test); err == nil {
+			t.Errorf("unescape(%s) had no error", test)
+		}
+	}
+}
