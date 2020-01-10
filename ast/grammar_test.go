6a58f8c6c4455c381b25cec25d225207ebd49b8a
Ethan Burns <burns.ethan@gmail.com>
2020-01-10 18:26:30 -0500

    fix associativity of binary operators to be left-associative, not right
    a *  b *  c, for any binary operator *, should group as (a *  b) *  c, but there was a long known bug in the parser where the associativity was reversed: a *  (b * c). This change fixes it and adds a few tests.

---
+++ ast/grammar_test.go
@@ -0,0 +1,45 @@
+package ast
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCallAssocPrec(t *testing.T) {
+	tests := []struct {
+		src string
+		// want is the paranthesized representation of the 1st statement.
+		want string
+	}{
+		{
+			src:  "func [f | a + b + c + d]",
+			want: "(((a) + (b)) + (c)) + (d)",
+		},
+		{
+			src:  "func [f | a b c d]",
+			want: "(((a) b) c) d",
+		},
+		{
+			src:  "func [f | a b + c d]",
+			want: "((a) b) + ((c) d)",
+		},
+		{
+			src:  "func [f | foo: a b + c bar: d]",
+			want: "foo: (((a) b) + (c)) bar: (d)",
+		},
+	}
+	for _, test := range tests {
+		p := NewParser("")
+		if err := p.Parse("", strings.NewReader(test.src)); err != nil {
+			t.Errorf("failed to parse [%s]: %s,", test.src, err.Error())
+			continue
+		}
+		stmt := p.Mod().Files[0].Defs[0].(*Fun).Stmts[0]
+		var s strings.Builder
+		stmt.buildString("", &s)
+		if s.String() != test.want {
+			t.Errorf("got:\n	%s\nexpected:\n	%s", s.String(), test.want)
+			continue
+		}
+	}
+}
