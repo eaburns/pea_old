1cc091e67a176339a0aed0d60d07e8165c6c54fd
Ethan Burns <burns.ethan@gmail.com>
2020-01-15 17:17:20 -0500

    add ast.ReadImports
    It returns just the imports for a source file.
    
    The current implementation is needlessly inefficient: it reads the entire source file, fully parses it, and returns the imports off of the AST. Since imports always come first, a more efficient implementation would read and parse just enough of the source file to get the imports and no more.

---
+++ ast/imports.go
@@ -0,0 +1,25 @@
+package ast
+
+import "io"
+
+// ReadImports returns all imports from the source given by an io.Reader.
+func ReadImports(r io.Reader) ([]string, error) {
+	// TODO: implement a more efficient ReadImports.
+	// The current implementation reads and fully parses
+	// the entire source file. However, imports always come first,
+	// so it should be easy to implement a variant of this
+	// that only reads and parses the beginning of the input.
+	p := NewParser("")
+	if err := p.Parse("", r); err != nil {
+		return nil, err
+	}
+
+	var paths []string
+	for _, f := range p.Mod().Files {
+		for _, imp := range f.Imports {
+			path := imp.Path[1 : len(imp.Path)-1] // trim "
+			paths = append(paths, path)
+		}
+	}
+	return paths, nil
+}
